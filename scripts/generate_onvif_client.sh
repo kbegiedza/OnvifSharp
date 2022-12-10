7646cca8f80002580fe2d81d9efca8b1dc9ec183
Krzysztof Begiedza <contact@kbegiedza.eu>
2022-12-10 10:34:15 +0000

    generate script update

--- scripts/generate_onvif_client.sh
+++ scripts/generate_onvif_client.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-ONVIF_SPEC_V10_FOLDER="/workspace/dep/onvif/specs/wsdl/ver10"
-ONVIF_SPEC_V20_FOLDER="/workspace/dep/onvif/specs/wsdl/ver20"
+ONVIF_SPEC_V10_DIRECTORY="/workspace/dep/onvif/specs/wsdl/ver10"
+ONVIF_SPEC_V20_DIRECTORY="/workspace/dep/onvif/specs/wsdl/ver20"
 
 ONVIF_NAMESPACE="OnvifSharp.Clients"
-CLIENTS_FOLDER="/workspace/src/${ONVIF_NAMESPACE}"
-CLIENTS_PROJECT="${CLIENTS_FOLDER}/${ONVIF_NAMESPACE}.csproj"
+CLIENTS_DIRECTORY="/workspace/src/${ONVIF_NAMESPACE}"
+CLIENTS_PROJECT="${CLIENTS_DIRECTORY}/${ONVIF_NAMESPACE}.csproj"
 
-# for FILE in $(find ${ONVIF_SPEC_V10_FOLDER} -name "*.wsdl")
-# do
-#     # echo ${FILE}
-#     NAME=$(basename -- ${FILE%.*})
-#     echo ${NAME}
-#     VERSION="V10"
-#     dotnet-svcutil -nl -d ${CLIENTS_FOLDER} -pf ${CLIENTS_PROJECT} -i -n "*,${ONVIF_NAMESPACE}.${VERSION}" -o "${NAME}Client${VERSION}.g.cs" ${FILE}
-# done
+function generate_all_from_directory() {
+    local DIRECTORY=${1}
+    local VERSION=${2}
+
+    for WSDL_FILE in $(find ${ONVIF_SPEC_V10_DIRECTORY} -name "*.wsdl")
+    do
+        CLIENT_NAME=$(basename -- ${WSDL_FILE%.*})
+        VERSION="V10"
+        dotnet-svcutil -nl -d ${CLIENTS_DIRECTORY} -pf ${CLIENTS_PROJECT} -i -n "*,${ONVIF_NAMESPACE}" -o "${CLIENT_NAME^}OnvifClient.g.cs" ${WSDL_FILE}
+    done
+}
